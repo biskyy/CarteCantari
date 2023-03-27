65a7bcd3f1fe2ec8a6064a8d3fdbae43765ff18c
biskyy <54770799+biskyy@users.noreply.github.com>
2023-03-28 01:02:19 +0300

    will now display the selected song content

--- CaietCantari/Components/SongList.js
+++ CaietCantari/Components/SongList.js
@@ -22,13 +22,14 @@ export default function SongList() {
     const buttonRef = useRef(null); // Defining a ref
 
     const handlePress = () => {
-      console.log(buttonRef.current.props.title); // Accessing the "title" attribute of the button using the ref
-      setCantare([true, 0]);
+      // console.log(buttonRef.current.props.id); // Accessing the "title" attribute of the button using the ref
+      setCantare([true, buttonRef.current.props.id]);
     };
 
     return (
       <Button
         ref={buttonRef} // Assigning the ref to the button component
+        id={props.id}
         title={props.title}
         onPress={handlePress}
         color={props.color}
@@ -46,9 +47,42 @@ export default function SongList() {
     <>
       {!veziCantare &&
         [...Cantari].map((cantare, index) => (
-          <ButonCantare key={index} title={cantare.name} color="#ff5c5c" />
+          <ButonCantare
+            key={index}
+            title={cantare.content
+              .split("\n")[1]
+              .replace(". ", cantare.id + ". ")
+              .slice(1)}
+            color="#ff5c5c"
+            id={cantare.id}
+          />
         ))}
-      {veziCantare && <Button onPress={backEvent} title="BACK" />}
+      {veziCantare && (
+        <>
+          <View>
+            <Text>
+              {Cantari[numarCantare - 1].content
+                .split("\n")[1]
+                .replace(". ", Cantari[numarCantare - 1].id + ". ")
+                .slice(1)}
+            </Text>
+            <ScrollView>
+              <Text>{Cantari[numarCantare - 1].content}</Text>
+            </ScrollView>
+            <Button onPress={backEvent} title="BACK" />
+          </View>
+        </>
+      )}
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  songContainer: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+  },
+  songTitle: {},
+});
