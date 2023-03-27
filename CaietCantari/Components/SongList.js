e6c333fef57987f7917a9e727d37d5d7cd1b48d5
biskyy <54770799+biskyy@users.noreply.github.com>
2023-03-27 22:51:05 +0300

    changed some values

--- CaietCantari/Components/SongList.js
+++ CaietCantari/Components/SongList.js
@@ -1,6 +1,6 @@
-import Cantari from "../Cantari.json"
- import React from "react";
- import {useState} from "react"
+import Cantari from "../Cantari.json";
+import React from "react";
+import { useState } from "react";
 import {
   Button,
   StyleSheet,
@@ -8,32 +8,44 @@ import {
   View,
   Platform,
   StatusBar,
-  Dimensions
+  ScrollView,
+  Dimensions,
 } from "react-native";
-const windowWidth = Dimensions.get('window').width;
+const windowWidth = Dimensions.get("window").width;
 
-export default function ToateCantarile(){
+export default function SongList() {
+  const [[cantare, numarCantare], setCantare] = useState([false, -1]);
 
-  const [[cantare,numarCantare], setCantare] = useState([true,-1])
+  const cantareEvent = (e) => {
+    setCantare([true, 0]);
+  };
 
-  const cantareEvent = (e) =>{
-   
-    setCantare([false,0]);
-    
-  }
+  const backEvent = () => {
+    setCantare([false, -1]);
+  };
 
-  const backEvent = () =>{
-    setCantare([true,-1]);
-  }
- 
+  const [[isSong, songID], setSong] = useState([false, 0]);
 
+  const displaySong = (index) => {
+    setSong([true, index]);
+  };
 
-    return(
-      <>
-       {cantare && [...Cantari].map( (cantare,index) => <Button id={index} onPress={cantareEvent} color="#ff5c5c" key={index} title={cantare.name}/>)  }
-       {!cantare && <Button onPress={backEvent} title="BACK"/>}
-       </>
-    )
+  return (
+    <>
+      {!cantare &&
+        [...Cantari].map((cantare, index) => (
+          <Button
+            id={index}
+            onPress={displaySong(cantare.id)}
+            color="#ff5c5c"
+            key={index}
+            title={cantare.name}
+          />
+        ))}
+      {cantare && [
+        ...Cantari.map((cantare, index) => <ScrollView></ScrollView>),
+      ]}
+      {cantare && <Button onPress={backEvent} title="BACK" />}
+    </>
+  );
 }
-
-
