b7041e091cf575bfadf3cafe68377187709278ac
biskyy <54770799+biskyy@users.noreply.github.com>
2023-04-02 14:53:40 +0300

    Fixed some major performance issues

--- CaietCantari/Components/SongList.js
+++ CaietCantari/Components/SongList.js
@@ -1,117 +1,83 @@
 import Cantari from "../Cantari.json";
-
-import React from "react";
-import { useState, useRef } from "react";
+import React, { useState, memo, useCallback, useMemo } from "react";
 import {
-  Button,
   StyleSheet,
   Text,
   View,
   Platform,
-  StatusBar,
-  ScrollView,
-  Dimensions,
-  Touchable,
-  TouchableOpacity,
+  FlatList,
+  Modal,
 } from "react-native";
-import Separator from "./Separator";
 import CustomButton from "./CustomButton";
+// import KeyboardShift from "@fullstackcraft/react-native-keyboard-shift/lib/components/KeyboardShift";
 
-export default function SongList() {
-  const [[veziCantare, numarCantare], setCantare] = useState([false, -1]);
-
-  const handlePress = (id) => {
-    // console.log(buttonRef.current.props.id); // Accessing the "title" attribute of the button using the ref
-    setCantare([true, id]);
-  };
-  ///////////////////////////////////////////////////////Cantare COmponent
-  const ButonCantare = (props) => {
-    const buttonRef = useRef(null); // Defining a ref
-
-    return (
-      <TouchableOpacity
-        style={{ color: "white" }}
-        ref={buttonRef} // Assigning the ref to the button component
-        id={props.id}
-        title={props.title}
-        onPress={() => handlePress(props.id)}
-        color={props.color}
-      >
-        <Text style={{ color: "white" }}>{props.title}</Text>
-      </TouchableOpacity>
-    );
-  };
-
-  ///////////////////////////////////////////////////////Cantare COmponent
+const SongList = memo(() => {
+  const [selectedSong, setSelectedSong] = useState([false, -1]);
 
   const backEvent = () => {
-    setCantare([false, -1]);
+    setSelectedSong([false, -1]);
+    console.log("pressed back");
   };
 
-  return (
-    <>
-      <View style={{ backgroundColor: "black", flex: 1 }}>
-        {!veziCantare && (
-          <>
-            <ScrollView
-              contentContainerStyle={{ flexGrow: 1 }}
-              indicatorStyle="white"
-            >
-              {[...Cantari].map((cantare, index) => (
-                <CustomButton
-                  key={index}
-                  text={cantare.content
-                    .split("\n")[1]
-                    .replace(". ", cantare.id + ". ")
-                    .slice(1)}
-                  textStyle={styles.songButtonText}
-                  textContainerStyle={styles.songButtonTextContainer}
-                  style={styles.songButton}
-                  onPress={() => handlePress(cantare.id)}
-                />
-              ))}
-            </ScrollView>
-          </>
-        )}
-        {veziCantare && (
-          <>
-            <View style={styles.songContainer}>
-              <View style={styles.songTitleContainer}>
-                <Text style={[styles.text, styles.titleText]}>
-                  {Cantari[numarCantare - 1].content
-                    .split("\n")[1]
-                    .replace(". ", Cantari[numarCantare - 1].id + ". ")
-                    .slice(1)}
-                </Text>
-              </View>
-              <Separator />
-              <View style={styles.songContentContainer}>
-                <ScrollView
-                  indicatorStyle="white"
-                  contentContainerStyle={styles.songScrollViewContainer}
-                >
-                  <Text style={[styles.text, styles.contentText]}>
-                    {Cantari[numarCantare - 1].content}
-                  </Text>
-                </ScrollView>
-              </View>
-              <View style={styles.songBackContainer}>
-                <Button
-                  onPress={backEvent}
-                  title="BACK"
-                  color={Platform.OS == "android" ? "black" : "white"}
-                />
-              </View>
-            </View>
-          </>
-        )}
-      </View>
-    </>
+  const renderSongItem = useCallback(({ item, index }) => {
+    console.log(index);
+    return (
+      <CustomButton
+        style={{}}
+        textStyle={{ color: "white" }}
+        onPress={() => setSelectedSong([true, item.content])}
+        text={item.content
+          .split("\n")[1]
+          .replace(". ", item.id + ". ")
+          .slice(1)}
+      />
+    );
+  }, []);
+
+  const renderSongContent = () => (
+    <Modal
+      style={{ marginTop: 100 }}
+      animationType="none"
+      visible={selectedSong[0]}
+      onRequestClose={() => console.log("test")}
+      transparent={false}
+    >
+      <CustomButton
+        onPress={backEvent}
+        text="Back"
+        style={{
+          alignItems: "center",
+          paddingVertical: 30,
+          marginTop: 100,
+          backgroundColor: "black",
+        }}
+      />
+      <Text style={{ color: "black" }}>{selectedSong[1]}</Text>
+    </Modal>
   );
-}
+
+  return (
+    <View>
+      <FlatList
+        data={Cantari}
+        renderItem={renderSongItem}
+        keyExtractor={useCallback((item, index) => index, [])}
+        style={styles.flatList}
+      />
+      {selectedSong && renderSongContent()}
+    </View>
+  );
+});
 
 const styles = StyleSheet.create({
-  songButton: { flex: 1, maxHeight: 40 },
+  flatList: {
+    backgroundColor: "black",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  songButton: { flex: 1, maxHeight: 40, minHeight: 40 },
   songButtonTextContainer: {
     flex: 1,
     marginLeft: 10,
@@ -152,4 +118,20 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   contentText: {},
+  bottomBar: {
+    width: "100%",
+    backgroundColor: "red",
+    marginBottom: Platform.OS == "ios" ? 20 : 0,
+  },
+  bottomBarTextContainerMainScreen: {
+    // flexDirection: "row",
+    backgroundColor: "purple",
+  },
+  bottomBarTextContainerSongScreen: {
+    flexDirection: "row",
+    backgroundColor: "purple",
+  },
+  bottomBarText: {},
 });
+
+export default SongList;
