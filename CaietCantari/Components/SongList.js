d0f0cad3a4e146b12ba8c4973df7da61b9f5e770
biskyy <54770799+biskyy@users.noreply.github.com>
2023-04-05 18:10:09 +0300

    Updated UI/styling

--- CaietCantari/Components/SongList.js
+++ CaietCantari/Components/SongList.js
@@ -1,5 +1,5 @@
 import Cantari from "../Cantari.json";
-import React, { useState, memo, useCallback, useMemo } from "react";
+import React, { useState, memo, useCallback, useMemo, useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,17 +7,34 @@ import {
   Platform,
   FlatList,
   Modal,
+  TextInput,
+  KeyboardAvoidingView,
+  Keyboard,
+  VirtualizedList,
+  ScrollView,
 } from "react-native";
 import CustomButton from "./CustomButton";
-// import KeyboardShift from "@fullstackcraft/react-native-keyboard-shift/lib/components/KeyboardShift";
+import KeyboardShift from "@fullstackcraft/react-native-keyboard-shift/lib/components/KeyboardShift";
+import NavBar from "./NavBar";
+import Separator from "./Separator";
 
 const songItemHeight = 40;
 
 const SongList = memo(() => {
-  const [selectedSong, setSelectedSong] = useState([false, -1]);
+  const [selectedSong, setSelectedSong] = useState([false, Cantari[0]]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [songs, setSongs] = useState(Cantari);
+  const [textSize, setTextSize] = useState(20);
+
+  const zoomIn = () => {
+    setTextSize(textSize + 2);
+  };
+  const zoomOut = () => {
+    if (textSize > 2) setTextSize(textSize - 2);
+  };
 
   const backEvent = () => {
-    setSelectedSong([false, -1]);
+    setSelectedSong([false, Cantari[0]]);
     console.log("pressed back");
   };
 
@@ -39,7 +56,7 @@ const SongList = memo(() => {
         style={styles.songButton}
         textContainerStyle={styles.songButtonTextContainer}
         textStyle={styles.songButtonText}
-        onPress={() => setSelectedSong([true, item.content])}
+        onPress={() => setSelectedSong([true, item])}
         text={item.content
           .split("\n")[1]
           .replace(". ", item.id + ". ")
@@ -48,41 +65,120 @@ const SongList = memo(() => {
     );
   }, []);
 
-  const renderSongContent = () => (
-    <Modal
-      style={{ marginTop: 100 }}
-      animationType="none"
-      visible={selectedSong[0]}
-      onRequestClose={() => console.log("test")}
-      transparent={false}
-    >
-      <CustomButton
-        onPress={backEvent}
-        text="Back"
-        style={{
-          alignItems: "center",
-          paddingVertical: 30,
-          marginTop: 100,
-          backgroundColor: "black",
-        }}
-      />
-      <Text style={{ color: "black" }}>{selectedSong[1]}</Text>
-    </Modal>
-  );
+  const renderSongContent = () => {
+    return (
+      <Modal
+        animationType="none"
+        visible={selectedSong[0]}
+        onRequestClose={() => console.log("test")}
+        transparent={false}
+        style={{ backgroundColor: "black" }}
+      >
+        <NavBar />
+        <View style={styles.songContainer}>
+          <View style={styles.songTitleContainer}>
+            <Text
+              style={[
+                styles.text,
+                styles.titleText,
+                { fontSize: textSize, flexShrink: 0 },
+              ]}
+              numberOfLines={1}
+            >
+              {selectedSong[1].content
+                .split("\n")[1]
+                .replace(". ", selectedSong[1].id + ". ")
+                .slice(1)}
+            </Text>
+          </View>
+          <Separator />
+          <View style={styles.songContentContainer}>
+            <ScrollView
+              indicatorStyle="white"
+              contentContainerStyle={styles.songScrollViewContainer}
+            >
+              <Text
+                style={[
+                  styles.text,
+                  styles.contentText,
+                  { fontSize: textSize },
+                ]}
+              >
+                {selectedSong[1].content}
+              </Text>
+            </ScrollView>
+          </View>
+        </View>
+        <Separator />
+        <View style={styles.songBackContainer}>
+          <View style={styles.bottomBarTextContainerSongScreen}>
+            <CustomButton
+              onPress={zoomOut}
+              textContainerStyle={{ justifyContent: "center", flex: 1 }}
+              text="Zoom Out"
+              style={styles.songContentButton}
+            />
+            <CustomButton
+              onPress={zoomIn}
+              textContainerStyle={{ justifyContent: "center", flex: 1 }}
+              text="Zoom In"
+              style={styles.songContentButton}
+            />
+            <View style={styles.songContentButton} />
+            <CustomButton
+              onPress={backEvent}
+              textContainerStyle={{ justifyContent: "center", flex: 1 }}
+              text="Back"
+              style={styles.songContentButton}
+            />
+          </View>
+        </View>
+      </Modal>
+    );
+  };
+
+  const onTextInputQueryChange = useCallback((query) => {
+    setSearchQuery(query);
+    // console.log(query);
+    // const filteredData = songs.filter((song) =>
+    //   song.id.toString().includes(query)
+    // );
+    // setSongs(filteredData);
+  }, []);
+
+  const renderFilteredList = () => {
+    <Modal animationType="none" transparent={false} visible={searchQuery != ""}>
+      <Text>Test</Text>
+    </Modal>;
+  };
 
   return (
-    <View>
+    <View style={styles.container}>
       <FlatList
         indicatorStyle="white"
-        data={Cantari}
+        data={songs}
         renderItem={renderSongItem}
         keyExtractor={getKeyItem}
         style={styles.flatList}
         initialNumToRender={900}
         maxToRenderPerBatch={450}
-        windowSize={150}
+        windowSize={900}
         getItemLayout={getItemLayout}
       />
+      {searchQuery != "" && renderFilteredList()}
+      <KeyboardAvoidingView
+        style={{ backgroundColor: "black" }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 68 : 0}
+      >
+        <TextInput
+          placeholder="Cauta o cantare"
+          placeholderTextColor="white"
+          value={searchQuery}
+          onChangeText={onTextInputQueryChange}
+          style={styles.textInput}
+        />
+      </KeyboardAvoidingView>
       {selectedSong && renderSongContent()}
     </View>
   );
@@ -110,28 +206,23 @@ const styles = StyleSheet.create({
     fontSize: 17,
   },
   songContainer: {
-    flex: 1,
-    alignItems: "center",
+    flex: 24,
     backgroundColor: "black",
   },
   songTitleContainer: {
-    flex: 1,
+    flexGrow: 1,
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
   },
   songContentContainer: {
-    flex: 20,
+    flex: 50,
     width: "100%",
+    backgroundColor: "black",
   },
   songScrollViewContainer: {
     flexGrow: 1,
     alignItems: "center",
-  },
-  songBackContainer: {
-    flex: 2,
-    marginBottom: 10,
-    justifyContent: "center",
   },
   text: {
     color: "white",
@@ -141,18 +232,43 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   contentText: {},
+  textInput: {
+    height: 50,
+    paddingLeft: 15,
+    backgroundColor: "black",
+    color: "white",
+    width: "100%",
+    marginBottom: Platform.OS == "ios" ? 21 : 0,
+    fontSize: 17,
+    borderWidth: 1,
+    borderColor: "white",
+    borderRadius: 10,
+  },
   bottomBar: {
     width: "100%",
     backgroundColor: "red",
-    marginBottom: Platform.OS == "ios" ? 20 : 0,
+    marginBottom: Platform.OS == "ios" ? 21 : 0,
   },
   bottomBarTextContainerMainScreen: {
     // flexDirection: "row",
     backgroundColor: "purple",
   },
+  songBackContainer: {
+    backgroundColor: "black",
+    flex: 2,
+    paddingBottom: Platform.OS == "ios" ? 21 : 0,
+  },
   bottomBarTextContainerSongScreen: {
+    flex: 1,
+    // alignItems: "center",
+    // backgroundColor: "purple",
     flexDirection: "row",
-    backgroundColor: "purple",
+  },
+  songContentButton: {
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: "center",
+    paddingVertical: 20,
   },
   bottomBarText: {},
 });
