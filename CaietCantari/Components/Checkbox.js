a699b8fe65fa1b46c7bc9d032c9c9b8da4c6b883
biskyy <54770799+biskyy@users.noreply.github.com>
2023-04-23 17:02:11 +0300

    New checkbox component

---
+++ CaietCantari/Components/Checkbox.js
@@ -0,0 +1,62 @@
+import React, { useMemo } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useAtom } from "jotai";
+import { themeAtom } from "./State";
+
+const CustomCheckbox = (props) => {
+  const [theme] = useAtom(themeAtom);
+  const txtColor = theme == "dark" ? "white" : "black";
+  const bgColor = theme == "dark" ? "black" : "white";
+
+  const styles = useMemo(
+    () =>
+      StyleSheet.create({
+        checkboxBase: {
+          margin: 5,
+          width: 32,
+          height: 32,
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: 4,
+          borderWidth: 2,
+          borderColor: txtColor,
+          backgroundColor: "transparent",
+        },
+        checkboxChecked: {
+          backgroundColor: txtColor,
+        },
+      }),
+    [theme]
+  );
+
+  return (
+    <TouchableOpacity onPress={props.onPress}>
+      <View
+        style={{
+          flexDirection: "row",
+          height: 42,
+          alignItems: "center",
+          backgroundColor: bgColor,
+          marginHorizontal: 10,
+          marginVertical: 5,
+          borderRadius: 4,
+          backgroundColor: "red",
+        }}
+      >
+        <View
+          style={[styles.checkboxBase, props.checked && styles.checkboxChecked]}
+        >
+          {props.checked && (
+            <Ionicons name="checkmark" size={25} color={bgColor} />
+          )}
+        </View>
+        <Text style={{ color: txtColor, fontWeight: 500, marginLeft: 5 }}>
+          {props.label}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomCheckbox;
