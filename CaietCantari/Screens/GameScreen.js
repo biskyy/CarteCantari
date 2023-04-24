05c8161bd1bf9d43155515b11b526d4bb95b3a9a
biskyy <54770799+biskyy@users.noreply.github.com>
2023-04-25 02:02:42 +0300

    2048 game

---
+++ CaietCantari/Screens/GameScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { StyleSheet, View } from "react-native";
+import GameController from "./Game/GameController";
+import { useAtom } from "jotai";
+import { themeAtom } from "../Components/State";
+
+const GameScreen = () => {
+  const [theme] = useAtom(themeAtom)
+  return (
+    <View style={{ flex: 1, backgroundColor: theme == "dark" ? "black" : "white" }}>
+      <GameController />
+    </View>
+  );
+};
+
+export default GameScreen;
